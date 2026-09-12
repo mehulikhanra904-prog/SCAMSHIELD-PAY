c4b2edc55def807433b63662813c7f655e7c161a
MEHULI KHANRA <mehulikhanra904@gmail.com>
2026-09-12 20:06:03 +0530

    Load threat profile dashboard styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./App.css";
 import "./history.css";
+import "./threat-profile.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
